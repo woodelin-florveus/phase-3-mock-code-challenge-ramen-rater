2a6f020d28110df5dd3619005f964539a14e62dd
Alex Marz <alexhmarz@gmail.com>
2021-02-09 14:56:18 -0500

    main deliverables complete

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
 const url = 'http://localhost:3000/ramens'
 const ramBar = document.querySelector('#ramen-menu')
 const ramDetail = document.querySelector('#ramen-detail')
-const ramRating = document.querySelector('#ramen-rating')
+ramDetail.innerHTML = ""
+const form = document.querySelector('#ramen-rating')
 let ramData 
 
 
@@ -20,7 +21,7 @@ function renderOneRamen(ramObj){
     // spam across top-bar
 
     const thisRamen = document.createElement('span')
-    thisRamen.setAttribute('ramen-id', ramObj.id)
+    thisRamen.setAttribute('data-ramen-id', ramObj.id)
     
     const thisRamenImg = document.createElement('img')
     thisRamenImg.src = ramObj.image
@@ -41,7 +42,7 @@ ramBar.addEventListener('click', ramView)
 
 function ramView(event){
 
-    const ramId = event.target.parentElement.getAttribute('ramen-id')
+    const ramId = event.target.parentElement.getAttribute('data-ramen-id')
     const ramIdVal = parseInt(ramId)
     console.log(ramIdVal)
 
@@ -53,59 +54,79 @@ function ramView(event){
 
 function renderRamInfo(ramObj){
     ramDetail.innerHTML = ""
-    ramRating.innerHTML = ""
-    const ramDiv = document.createElement('div')
-    ramDiv.setAttribute('ramDiv-id', ramObj.id)
+    form.innerHTML = ""
+    // const ramDiv = document.createElement('div')
+    // ramDiv.setAttribute('data-ramDiv-id', ramObj.id)
 
-    ramDiv.innerHTML = `<img class="detail-image" src= "${ramObj.image}" alt="${ramObj.name}"/>
+    ramDetail.innerHTML = `<img class="detail-image" src= "${ramObj.image}" alt="${ramObj.name}"/>
     <h2 class="name">${ramObj.name}</h2>
     <h3 class="restaurant">${ramObj.restaurant}</h3>`
 
     // deliverable 2 add existing rating and comment
 
-    const div = document.createElement('div')
-    div.setAttribute('div-id', ramObj.id) 
-    
-    div.innerHTML = `<label for="rating">Rating: </label>
+    // const div = document.createElement('div')
+    // div.setAttribute('data-div-id', ramObj.id) 
+    form.setAttribute('data-id', ramObj.id)
+    form.innerHTML = `<label for="rating">Rating: </label>
     <input type="text" name="rating" id="rating" value="${ramObj.rating}" />
     <label for="comment">Comment: </label>
     <textarea name="comment" id="comment">${ramObj.comment}</textarea>
     <input type="submit" value="Update" />`
 
-    ramRating.append(div) 
-    ramDetail.append(ramDiv)
-    
+    // ramRating.append(div) 
+    // ramDetail
 }
-
-ramRating.addEventListener('submit', updateRamInfo)
+//const form = document.getElementById('ramen-rating')
+console.log(form)
+form.addEventListener('submit', updateRamInfo)
 
 function updateRamInfo(event){
-
     event.preventDefault();
+    console.log(event.target.dataset.id)
+    const ramen = event.target.dataset.id
 
-    const ramen = event.target.firstChild
-    const ramenId = ramen.getAttribute('div-id')
-    const ramenIdVal = parseInt(ramenId)
-    // console.log(ramenIdVal)
+     const rating = event.target.rating.value
+     const comment = event.target.comment.value
+    console.log(comment, rating)
 
-    // console.log(event.target.firstChild.element[0])
+    const updatedRamen = {rating, comment}
+    // newUpdate(ramen, updatedRamen)
+    
+    // function newUpdate(obj, updatedRamen){
+        fetch(`${url}/${ramen}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json", 
+                Accept: "application/json",
+            },
+            body: JSON.stringify(updatedRamen)
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+    
+    }
 
-    const ramNumRating = event.target.firstChild.childNodes
 
-    const ramNumRatingVal = ramNumRating[2].value
 
-    const ramCommentVal = ramNumRating[6].textContent.value
+    // const ramenId = ramen.getAttribute('div-id')
+    // const ramenIdVal = parseInt(ramenId)
+    // // console.log(ramenIdVal)
 
-    ramRating.rating.value = ramNumRatingVal
+    // // console.log(event.target.firstChild.element[0])
 
-    // console.log(ramNumRatingVal)
+    // const ramNumRating = event.target.firstChild.childNodes
+    // const ramNumRatingVal = ramNumRating[2].value
 
-    // create fetch 
+    // const ramCommentVal = ramNumRating[6].innerHTML
+    // console.log(ramCommentVal)
+    // console.log(event.target.firstChild.querySelector('#comment').innerHTML)
 
-    fetch(`${url}/${ramenIdVal}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-}
+    // // create fetch 
+
+    // fetch(`${url}/${ramenIdVal}`)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+
 
 // function editRamen(ramObj){
 //     const 
